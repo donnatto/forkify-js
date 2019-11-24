ecd5704e91ba6d13a278db5837682991ab876ccb
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-24 12:49:17 -0500

    implement persistence with localstorage

--- src/js/index.js
+++ src/js/index.js
@@ -116,10 +116,6 @@ elements.shopping.addEventListener('click', e => {
 });
 
 // LIKE CONTROLLER
-// Testing
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
   if (!state.likes) state.likes = new Likes();
   const currentID = state.recipe.id;
@@ -147,6 +143,17 @@ const controlLike = () => {
   }
   likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+  state.likes = new Likes();
+  // Restore likes
+  state.likes.readStorage();
+  // Toggle like menu button
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
+  // Render the existing likes
+  state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
